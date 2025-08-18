312e3c1c420be899badfc6dda1c842519999a623
antenickawest-png <antenicka.west@rnstower.com>
2025-08-18 17:41:40 -0500

    Update providers.tsx

--- supabase-scheduler-10/app/providers.tsx
+++ supabase-scheduler-10/app/providers.tsx
@@ -1,1 +1,8 @@
-h
+"use client";
+
+import type React from "react";
+import { AuthProvider } from "../hooks/use-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <AuthProvider>{children}</AuthProvider>;
+}
