d3d6fc98f58412a5374f44ce94ff2042c8bf8f1d
antenickawest-png <antenicka.west@rnstower.com>
2025-08-21 15:42:11 -0500

    Delete supabase-scheduler-10/app directory

--- supabase-scheduler-10/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import type React from "react";
-import { AuthProvider } from "../hooks/use-auth";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
